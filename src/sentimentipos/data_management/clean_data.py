18b109f784c8a44b8e43e02134a8cbe59d0efff3
s6lulisc <s6lulisc@uni-bonn.de>
2023-03-25 17:09:45 +0100

    I added AVLR to the ipo_tickers function in clean_data.py

--- src/sentimentipos/data_management/clean_data.py
+++ src/sentimentipos/data_management/clean_data.py
@@ -47,7 +47,7 @@ def ipo_tickers():
         by who is performing the analysis.
 
     """
-    ipo_tickers = ["DBX", "SPOT", "EQH"]
+    ipo_tickers = ["DBX", "SPOT", "EQH", "AVLR"]
     return ipo_tickers
 
 
