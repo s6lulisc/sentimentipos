4fbc24a2b3f3461ae0f59543b3b687a9a0f9df66
leonardorotasperti <s6lerota@uni-bonn.de>
2023-03-25 16:30:12 +0100

    added docstrings to the model.py script

--- src/sentimentipos/analysis/model.py
+++ src/sentimentipos/analysis/model.py
@@ -3,6 +3,25 @@ import statsmodels.api as sm
 
 
 def get_sentiment_scores(ipo_list, lm, path):
+    """Calculates sentiment scores for a list of IPO tickers using a given language
+    model.
+
+    Args:
+        ipo_list (list): A list of stock tickers to calculate sentiment scores for.
+        lm (LanguageModel): The language model to use for sentiment analysis.
+        path (Path): The path to the directory containing the CSV files with the tokenized
+            words for each company to use in the sentiment analysis.
+
+    Returns:
+        df_scores (pandas.DataFrame): A DataFrame with sentiment scores for each ticker in ipo_list.
+            The DataFrame has the following columns:
+            - Positive: The proportion of words with a positive sentiment score.
+            - Negative: The proportion of words with a negative sentiment score.
+            - Polarity: The overall polarity score (positive - negative).
+            - Subjectivity: The overall subjectivity score (proportion of words with
+              non-neutral sentiment).
+
+    """
     # Initialize an empty DataFrame with the tickers as index
     df_scores = pd.DataFrame(index=ipo_list)
 
@@ -24,8 +43,19 @@ def get_sentiment_scores(ipo_list, lm, path):
 
 
 def run_linear_regression(df_info, sentiment_scores):
-    # Concatenate X and y into a single DataFrame
-    # Print the model summary
+    """Runs a linear regression model using sentiment scores as the independent variable
+    and IPO first day returns as the dependent variable.
+
+    Args:
+        df_info (pandas.DataFrame): A DataFrame containing IPO information, including
+            the ticker, company name, IPO date, and stock returns.
+        sentiment_scores (pandas.DataFrame): A DataFrame containing sentiment scores for
+            each ticker in df_info.
+
+    Returns:
+        summary_table (str): A summary table of the linear regression model.
+
+    """
     df_info.reset_index(drop=True, inplace=True)
     sentiment_scores.reset_index(drop=True, inplace=True)
     y = df_info["returns"]
