0aac9e2e0078e8b22ef3910bcafe0a77a4ec94f9
s6lulisc <115686936+s6lulisc@users.noreply.github.com>
2023-03-30 16:43:16 +0200

    we updated the depends_on and produces in the task files such that it is now in a better and cleaner format.

--- src/sentimentipos/analysis/task_analysis.py
+++ src/sentimentipos/analysis/task_analysis.py
@@ -6,23 +6,15 @@ from sentimentipos.config import BLD
 from sentimentipos.data_management import ipo_tickers
 
 
-@pytask.mark.depends_on(
-    {
-        "data_info": BLD / "python" / "data" / "tokenized_texts",
-    },
-)
-@pytask.mark.produces(
-    {
-        "models": BLD / "python" / "models",
-    },
-)
+@pytask.mark.depends_on(BLD / "python" / "data" / "tokenized_texts")
+@pytask.mark.produces(BLD / "python" / "models" / "sentiment_scores.csv")
 def task_get_sentiment_scores(depends_on, produces):
-    """Use models/tables for regression plot, save as .png."""  ### need to rewrite this docstring
+    """Use models/tables for regression plot, save as .png."""
     lm = ps.LM()
     ipo_list = ipo_tickers()
     sentiment_scores = get_sentiment_scores(
         ipo_list,
         lm,
-        depends_on["data_info"],
+        depends_on,
     )
-    sentiment_scores.to_csv(produces["models"] / "sentiment_scores.csv")
+    sentiment_scores.to_csv(produces)
