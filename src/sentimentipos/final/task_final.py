edf9cef569cc47bc60d5ba8ae2a533e250e9967f
s6lulisc <115686936+s6lulisc@users.noreply.github.com>
2023-03-31 21:42:18 +0200

    we changed a few function arguments to make the code more fluid and clean. Also changed docstrings. Added more content to the paper. And modified tests.

--- src/sentimentipos/final/task_final.py
+++ src/sentimentipos/final/task_final.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import pytask
-import statsmodels.api as sm
 
 from sentimentipos.analysis import run_linear_regression
 from sentimentipos.config import BLD
@@ -20,15 +19,16 @@ from sentimentipos.final import plot_regression
         "tables": BLD / "python" / "tables",
     },
 )
-def task_regression_plot(depends_on, produces):
+def task_regression_figure_table(depends_on, produces):
+    """Saves regression summary table as a LaTeX file and plots the model."""
     sentiment_scores = pd.read_csv(depends_on["models"])
     ipo_info = pd.read_csv(depends_on["ipo_info_data"])
-    summary_table = run_linear_regression(ipo_info, sentiment_scores)
-    with open(produces["tables"] / "summary_table.tex", "w") as f:
-        f.write(summary_table.as_latex())
-
+    model = run_linear_regression(ipo_info, sentiment_scores)
     y = ipo_info["returns"]
     X = sentiment_scores["Polarity"]
-    model = sm.OLS(y, sm.add_constant(X)).fit()
     plot_regression(X, y, model, ipo_info)
     plt.savefig(produces["figures"])
+
+    summary_table = model.summary()
+    with open(produces["tables"] / "summary_table.tex", "w") as f:
+        f.write(summary_table.as_latex())
